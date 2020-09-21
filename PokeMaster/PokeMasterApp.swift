9569c19058d171e6d65c4fed891afbfe0b2f0593
Chen Yuanbing <373381941@qq.com>
2020-09-21 10:28:05 +0800

    Initial Commit

---
+++ PokeMaster/PokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMasterApp.swift
+//  PokeMaster
+//
+//  Created by Dsee.Lab on 2020/9/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
